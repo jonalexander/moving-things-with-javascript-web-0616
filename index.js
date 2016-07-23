af408f534cfa82664fa8167c0b35b60223fbeee7
jonalexander <jonathandalexander@gmail.com>
2016-07-23 14:47:27 -0400

    done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+    dodger.style.left = `${left + 1}px`
+  }
+})
+
+//refactored
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (if left < 461) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
